2567377342f05a0b1e26dfbc7cf6ca12e5a0501b
Arthur <arthur@corenzan.com>
2018-10-08 19:14:06 -0300

    Overhaul of the twitter client

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -4,137 +4,180 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"strings"
 	"time"
 
-	"github.com/corenzan/parrot/helpers"
 	"github.com/patrickmn/go-cache"
 )
 
-var (
-	ua            = "Parrot/1.0"
-	endpoint      = "https://api.twitter.com"
-	activityCache *cache.Cache
-	logger        *log.Logger
-	credentials   string
-	client        *http.Client
+const (
+	ua       = "Parrot/1.0"
+	endpoint = "https://api.twitter.com"
 )
 
-// https://developer.twitter.com/en/docs/basics/authentication/overview/application-only#issuing-application-only-requests
-func token() string {
-	url := endpoint + "/oauth2/token"
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString("grant_type=client_credentials"))
-	if err != nil {
-		logger.Println("token() failed:", err)
-		return ""
+// Activity ...
+type Activity struct {
+	Status string `json:"status"`
+}
+
+// HTML ...
+func (a *Activity) HTML() string {
+	return a.Status
+}
+
+// Text ...
+func (a *Activity) Text() string {
+	return a.Status
+}
+
+// Client ...
+type Client struct {
+	credentials string
+	cache       *cache.Cache
+	http        *http.Client
+}
+
+// New ...
+func New(key, secret string) *Client {
+	return &Client{
+		http: &http.Client{
+			Timeout: time.Second * 2,
+		},
+		cache:       cache.New(1*time.Hour, 1*time.Hour),
+		credentials: base64.StdEncoding.EncodeToString([]byte(key + ":" + secret)),
 	}
-	req.Header.Add("Authorization", "Basic "+credentials)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
-	req.Header.Add("User-Agent", ua)
-	resp, err := client.Do(req)
+}
+
+// NewRequest ...
+func (c *Client) NewRequest(method, path string, body io.Reader) (*http.Request, error) {
+	u, err := url.Parse(endpoint + path)
 	if err != nil {
-		logger.Println("token() failed:", err)
-		return ""
+		return nil, err
+	}
+	req, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("User-Agent", ua)
+	return req, nil
+}
+
+// AccessToken ...
+func (c *Client) AccessToken() (string, error) {
+	req, err := c.NewRequest("POST", "/oauth2/token", bytes.NewBufferString("grant_type=client_credentials"))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Authorization", "Basic "+c.credentials)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return "", err
 	}
 	if resp.StatusCode != http.StatusOK {
-		logger.Println("token() failed:", resp.Status)
-		return ""
+		return "", errors.New(resp.Status)
 	}
 	var token map[string]string
 	err = json.NewDecoder(resp.Body).Decode(&token)
 	if err != nil {
-		logger.Println("token() failed:", err)
-		return ""
+		return "", err
 	}
 	if token["token_type"] != "bearer" {
-		logger.Printf("token() failed: invalid token type: %v", token)
-		return ""
+		return "", errors.New("Token type is not bearer")
 	}
-	return token["access_token"]
+	return token["access_token"], nil
 }
 
-func latestActivity(username string) string {
-	url := endpoint + "/1.1/statuses/user_timeline.json?count=1&screen_name=" + username
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		logger.Println("latestActivity() failed:", err)
-		return ""
+// Twit ...
+type Twit struct {
+	Text string `json:"text"`
+}
+
+// Timeline ...
+type Timeline []Twit
+
+// LatestActivity ...
+func (c *Client) LatestActivity(username string) (*Activity, error) {
+	var activity *Activity
+	if value, ok := c.cache.Get(username); ok {
+		activity = value.(*Activity)
+	} else {
+		req, err := c.NewRequest("GET", "/1.1/statuses/user_timeline.json?count=1&screen_name="+username, nil)
+		if err != nil {
+			return nil, err
+		}
+		token, err := c.AccessToken()
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Add("Authorization", "Bearer "+token)
+		resp, err := c.http.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode != http.StatusOK {
+			return nil, errors.New(resp.Status)
+		}
+		timeline := Timeline{}
+		err = json.NewDecoder(resp.Body).Decode(&timeline)
+		if err != nil {
+			return nil, err
+		}
+		activity = &Activity{
+			Status: timeline[0].Text,
+		}
+		c.cache.Set(username, activity, cache.DefaultExpiration)
 	}
-	token := token()
-	if token == "" {
-		return ""
-	}
-	req.Header.Add("Authorization", "Bearer "+token)
-	req.Header.Add("User-Agent", ua)
-	resp, err := client.Do(req)
-	if err != nil {
-		logger.Println("latestActivity() failed:", err)
-		return ""
-	}
-	if resp.StatusCode != http.StatusOK {
-		logger.Println("latestActivity() failed:", resp.Status)
-		return ""
-	}
-	statuses := make([]map[string]interface{}, 1)
-	err = json.NewDecoder(resp.Body).Decode(&statuses)
-	if err != nil {
-		logger.Println("latestActivity() failed:", err)
-		return ""
-	}
-	return statuses[0]["text"].(string)
+	return activity, nil
+}
+
+var (
+	client *Client
+)
+
+func init() {
+	secret := os.Getenv("TWITTER_SECRET")
+	key := os.Getenv("TWITTER_KEY")
+
+	client = New(key, secret)
 }
 
 // ServeHTTP ...
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ext := path.Ext(r.URL.Path)
-	name := strings.TrimSuffix(path.Base(r.URL.Path), ext)
-	var activity string
-	if value, found := activityCache.Get(name); found {
-		activity = value.(string)
-	} else {
-		activity = latestActivity(name)
-		activityCache.Set(name, activity, cache.DefaultExpiration)
-	}
-	if activity == "" {
-		w.WriteHeader(http.StatusInternalServerError)
+	basename := path.Base(r.URL.Path)
+	ext := path.Ext(basename)
+	username := strings.TrimSuffix(basename, ext)
+	if username == "" {
+		http.NotFound(w, r)
 		return
 	}
+
+	activity, err := client.LatestActivity(username)
+	if err != nil {
+		panic(err)
+	}
+
 	switch ext {
 	case ".html", "":
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, helpers.AutoLink(activity))
+		fmt.Fprint(w, activity.HTML())
 	case ".json":
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		status := map[string]string{
-			"status": activity,
-		}
-		err := json.NewEncoder(w).Encode(&status)
+		err := json.NewEncoder(w).Encode(activity)
 		if err != nil {
-			logger.Println("ServeHTTP() failed:", err)
+			panic(err)
 		}
 	case ".txt":
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, activity)
+		fmt.Fprint(w, activity.Text())
 	default:
 		w.WriteHeader(http.StatusNotAcceptable)
 	}
-}
-
-func init() {
-	logger = log.New(os.Stdout, "twitter: ", log.LstdFlags)
-	client = &http.Client{
-		Timeout: time.Second * 2,
-	}
-	activityCache = cache.New(1*time.Hour, 1*time.Hour)
-	key := os.Getenv("TWITTER_KEY")
-	secret := os.Getenv("TWITTER_SECRET")
-	credentials = base64.StdEncoding.EncodeToString([]byte(key + ":" + secret))
 }
